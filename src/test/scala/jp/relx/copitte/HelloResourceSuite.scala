44c04fbbd97f14cfe0e992566e327a67441ea89d
Yamanaka Yuu <yuu@relx.jp>
2012-05-04 12:49:35 +0900

    REST API のテストサンプルを追加
    HelloResource の hello メソッドのテスト

---
+++ src/test/scala/jp/relx/copitte/HelloResourceSuite.scala
@@ -0,0 +1,28 @@
+package jp.relx.copitte
+import org.scalatest.fixture.FunSuite
+import org.apache.wink.client.RestClient
+import org.apache.wink.client.Resource
+import java.net.URI
+import scala.xml.XML
+
+class RepositoryResourceSuite extends FunSuite {
+
+  type FixtureParam = (URI) => Resource
+
+  override def withFixture(test: OneArgTest) {
+    test { (path: URI) =>
+      new RestClient().resource(
+        TestConfigs.AppContextUri.resolve(path))
+    }
+  }
+
+  test("hello test") { handler =>
+    val res = handler(new URI("/")).get()
+    expect(200) { res.getStatusCode() }
+
+    val body = XML.loadString(res.getEntity(classOf[String]))
+    expect("Hello, World") {
+      body \ "body" text
+    }
+  }
+}
---
+++ src/test/scala/jp/relx/copitte/TestConfigs.scala
@@ -0,0 +1,6 @@
+package jp.relx.copitte
+import java.net.URI
+
+object TestConfigs {
+  val AppContextUri = new URI("http://localhost:8080")
+}
